fcd01fca82750270db72ada1bf764b1a2b5cb998
Raros17 <sookmeoyhj@gmail.com>
2024-05-02 00:22:41 +0900

    fix: Server package.json 및 TS 문제 해결

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,20 +4,16 @@ import express from 'express';
 const app = express();
 const PORT = 8080;
 
+app.get('/', (req, res) => {
+    res.send('hello!');
+});
+
 app.get('/crawl', async (req, res) => {
-    res.send('냐앙')
-    // try {
-    //     const data = await crawls(); // 크롤링 실행
-    //     res.json({ data }); // JSON 형식으로 응답
-    //   } catch (err) {
-    //     console.error("Error during crawling:", err); // 오류 로깅
-    //     res.status(500).json({ error: err.message }); // 오류 응답
-    //   }
+  res.send('냐앙');
 });
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`); 
-  // 서버가 정상적으로 실행되면 이 메시지가 출력됩니다.
 });
 
 
