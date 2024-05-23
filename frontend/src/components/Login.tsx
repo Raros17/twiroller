d7fc551d4ff35ecc058f44139e5a5f74b2c6b5e8
Raros17 <sookmeoyhj@gmail.com>
2024-05-23 23:30:24 +0900

    feat: Link와 Router 추가

---
+++ frontend/src/components/Login.tsx
@@ -0,0 +1,15 @@
+function Login() {
+  const handleTwitterLogin = () => {
+    // 트위터 로그인을 시도하는 로직 추가
+    // 예: window.location.href = '/twitter-login';
+  };
+
+  return (
+    <div style={{backgroundColor: "red"}}>
+      <h2>트위터로 로그인 하기</h2>
+      <button>로그인하기</button>
+    </div>
+  );
+}
+
+export default Login;
