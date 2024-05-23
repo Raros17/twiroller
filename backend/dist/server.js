7508749e2627d6c201838b8cae00552dcbc6bdac
Raros17 <sookmeoyhj@gmail.com>
2024-05-23 15:31:38 +0900

    feat: Oauth 트위터 로그인 서버 로직

--- backend/dist/server.js
+++ backend/dist/server.js
@@ -11,10 +11,74 @@ import { tweetCrawler } from './scripts/tweetCrawler.js';
 import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import axios from 'axios';
 const app = express();
 const PORT = 8080;
 app.use(cors());
 app.use(bodyParser.json());
+app.use(cookieParser());
+const consumerKey = 'YOUR_CONSUMER_KEY';
+const consumerSecret = 'YOUR_CONSUMER_SECRET';
+const callbackURL = 'http://localhost:3000/callback';
+let requestTokenSecret = null;
+//인증요청
+app.get('/login', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const response = yield axios.post('https://api.twitter.com/oauth/request_token', null, {
+            params: {
+                oauth_callback: callbackURL
+            },
+            headers: {
+                Authorization: `OAuth oauth_consumer_key="${consumerKey}", oauth_signature_method="HMAC-SHA1", oauth_timestamp="${Math.floor(Date.now() / 1000)}", oauth_nonce="${Math.random().toString(36).substring(7)}", oauth_version="1.0"`
+            }
+        });
+        const requestToken = new URLSearchParams(response.data);
+        requestTokenSecret = requestToken.get('oauth_token_secret');
+        res.redirect(`https://api.twitter.com/oauth/authenticate?oauth_token=${requestToken.get('oauth_token')}`);
+    }
+    catch (error) {
+        res.status(500).send('Error getting request token');
+    }
+}));
+// 트위터 콜백 처리
+app.get('/callback', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { oauth_token, oauth_verifier } = req.query;
+    try {
+        const response = yield axios.post('https://api.twitter.com/oauth/access_token', null, {
+            params: {
+                oauth_token,
+                oauth_verifier
+            },
+            headers: {
+                Authorization: `OAuth oauth_consumer_key="${consumerKey}", oauth_token="${oauth_token}", oauth_signature_method="HMAC-SHA1", oauth_timestamp="${Math.floor(Date.now() / 1000)}", oauth_nonce="${Math.random().toString(36).substring(7)}", oauth_version="1.0"`
+            }
+        });
+        const accessToken = new URLSearchParams(response.data);
+        const oauthToken = accessToken.get('oauth_token');
+        const oauthTokenSecret = accessToken.get('oauth_token_secret');
+        res.cookie('oauth_token', oauthToken, { httpOnly: true });
+        res.cookie('oauth_token_secret', oauthTokenSecret, { httpOnly: true });
+        res.redirect('/profile');
+    }
+    catch (error) {
+        res.status(500).send('Error getting access token');
+    }
+}));
+app.get('/profile', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { oauth_token, oauth_token_secret } = req.cookies;
+    try {
+        const response = yield axios.get('https://api.twitter.com/1.1/account/verify_credentials.json', {
+            headers: {
+                Authorization: `OAuth oauth_consumer_key="${consumerKey}", oauth_token="${oauth_token}", oauth_token_secret="${oauth_token_secret}", oauth_signature_method="HMAC-SHA1", oauth_timestamp="${Math.floor(Date.now() / 1000)}", oauth_nonce="${Math.random().toString(36).substring(7)}", oauth_version="1.0"`
+            }
+        });
+        res.json(response.data);
+    }
+    catch (error) {
+        res.status(500).send('Error fetching user profile');
+    }
+}));
 let crawledData = null;
 app.get('/', (req, res) => {
     res.send('hello!');
