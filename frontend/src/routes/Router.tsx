26ae3b110ee4a876e9a756575dda44dc1028e51c
Raros17 <sookmeoyhj@gmail.com>
2024-05-26 21:18:43 +0900

    feat: info 페이지 설명 추가

--- frontend/src/routes/Router.tsx
+++ frontend/src/routes/Router.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from '../components/Home';
 import Login from '../components/Login';
+import Info from '../components/Info';
 
 function Router() {
   return (
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/info" element={<Info/>} />
       </Routes>
   );
 }
