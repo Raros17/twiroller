895a42a7b6ee5cade4cd03885c4b64c1eb4db8b2
Raros17 <sookmeoyhj@gmail.com>
2024-06-07 00:06:01 +0900

    design: AppContainer 영역 추가

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,16 +3,25 @@ import { BrowserRouter } from 'react-router-dom';
 import Router from './routes/Router';
 import Footer from './components/Footer';
 import Header from './components/Header'
+import styled from 'styled-components';
 
 function App() {
   return (
     <>
     <BrowserRouter> 
-      <Header/>   
-      <Router/>
-      <Footer/>
+      <AppContainer>
+        <Header/>
+        <Router/>
+        <Footer/>
+      </AppContainer>
     </BrowserRouter>        
     </>
   )
 }
 export default App;
+
+const AppContainer = styled.section`
+  height: 100%;
+  position: relative;
+  padding-bottom: 80px;
+`
