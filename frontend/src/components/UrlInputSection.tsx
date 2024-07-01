5d0b127182dd54c675c91805eedc4f3ca3ee36f4
Raros17 <sookmeoyhj@gmail.com>
2024-07-01 22:27:38 +0900

    feat: keyDown Enter event 처리

--- frontend/src/components/UrlInputSection.tsx
+++ frontend/src/components/UrlInputSection.tsx
@@ -1,23 +1,35 @@
-import React, {RefObject} from 'react';
+import React, { RefObject, KeyboardEvent } from 'react';
 import { InputWrap, HyperLinkInput, InputTextDeleteBtn, CrawlingBtn, ErrorMessageSection, InputSection } from './Home.styles';
 
-
 interface UrlInputSectionProps {
-    urlInput: RefObject<HTMLInputElement>;
-    clearInputField: () => void;
-    handleSubmit: () => void;
-    errorMessage: string; 
-  }
-  
-const UrlInputSection:React.FC<UrlInputSectionProps> = ({ urlInput, clearInputField, handleSubmit, errorMessage }) => (
-  <InputSection>
-    <ErrorMessageSection>{errorMessage && <p>{errorMessage}</p>} </ErrorMessageSection>
-    <InputWrap>
-        <HyperLinkInput type="text" ref={urlInput} />
+  urlInput: RefObject<HTMLInputElement>;
+  clearInputField: () => void;
+  handleSubmit: () => void;
+  errorMessage: string;
+}
+
+const UrlInputSection: React.FC<UrlInputSectionProps> = ({ urlInput, clearInputField, handleSubmit, errorMessage }) => {
+
+    const handleKeyDown = (event:KeyboardEvent<HTMLInputElement>) => {
+        if (event.key === 'Enter'){
+            handleSubmit();
+        }
+    }
+
+  return (
+    <InputSection>
+      {errorMessage && (
+        <ErrorMessageSection>
+          <p>{errorMessage}</p>
+        </ErrorMessageSection>
+      )}
+      <InputWrap>
+        <HyperLinkInput type="text" ref={urlInput} onKeyDown={handleKeyDown} placeholder='최하단 Tweet URL을 입력하세요.'/>
         <InputTextDeleteBtn onClick={clearInputField}>x</InputTextDeleteBtn>
-        <CrawlingBtn onClick={handleSubmit}>추출하기</CrawlingBtn>        
-    </InputWrap>
-  </InputSection>
-);
+        <CrawlingBtn onClick={handleSubmit} >추출하기</CrawlingBtn> 
+      </InputWrap>
+    </InputSection>
+  );
+};
 
 export default UrlInputSection;
