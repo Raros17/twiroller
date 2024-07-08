a71718b5fbb4172e919db1e9db126451d5b106eb
Raros17 <sookmeoyhj@gmail.com>
2024-07-08 23:51:04 +0900

    chore: recoil 설치 및 세팅

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,25 @@
-import './styles/global-style.css';
-import { BrowserRouter } from 'react-router-dom';
-import Router from './routes/Router';
-import Footer from './components/Footer';
-import Header from './components/Header'
-import styled from 'styled-components';
+import "./styles/global-style.css";
+import { BrowserRouter } from "react-router-dom";
+import Router from "./routes/Router";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import styled from "styled-components";
+import { RecoilRoot } from "recoil";
 
 function App() {
   return (
-    <>
-    <BrowserRouter>         
-    <Header/>
-      <AppContainer>
-        <MainContent>
-          <Router/>
-        </MainContent>        
-      </AppContainer>
-      <Footer/>
-    </BrowserRouter>        
-    </>
-  )
+    <RecoilRoot>
+      <BrowserRouter>
+        <Header />
+        <AppContainer>
+          <MainContent>
+            <Router />
+          </MainContent>
+        </AppContainer>
+        <Footer />
+      </BrowserRouter>
+    </RecoilRoot>
+  );
 }
 export default App;
 const AppContainer = styled.div`
@@ -30,5 +31,5 @@ const AppContainer = styled.div`
 
 const MainContent = styled.main`
   flex: 1 0 auto;
-  overflow-y: auto;  
+  overflow-y: auto;
 `;
